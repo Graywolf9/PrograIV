3f171f0bd270f1087bf2c15f840736e57e43e605
Your Name <you@example.com>
2015-11-29 22:27:22 +0000

    2

---
+++ page/contacto.php
@@ -0,0 +1,10 @@
+<!DOCTYPE html>
+<HTML>
+	<HEAD>
+	</HEAD>
+	<BODY>
+		<DIV>
+			<A href="#"><IMG style:"height: 48px" src="http://logovector.net/wp-content/uploads/2014/01/318464-facebook-5-logo.png"></A><p>Telefono: (33)3770 3300 ext. 25143<br>Aula B-308</p>
+		</DIV>
+	</BODY>
+</HTML>
---
+++ page/coordinacion.php
@@ -0,0 +1,36 @@
+<!DOCTYPE html>
+<HTML>
+    <HEAD>
+    </HEAD>
+	<BODY>
+		<DIV>
+			<TABLE>
+				<TR>
+					<TD>
+						<img src="http://cucea.udg.mx/sites/default/files/imagenes/directorio/ing._irene_huizar_navarro.jpg">
+					</TD>
+					<TD>
+						<p>Irene Huizar Navarro</p>
+					</TD>
+				</TR>
+				<TR>
+					<TD>
+						<img src="">
+					</TD>
+					<TD>
+						<p>Andrea Leyva Lozano</p>
+					</TD>
+				</TR>
+				<TR>
+					<TD>
+						<img src="">
+					</TD>
+					<TD>
+						<p>Susana Elizabeth Mora Quirarte</p>
+					</TD>
+				</TR>
+			</TABLE>
+		</DIV>
+	</BODY>
+</HTML>
+
